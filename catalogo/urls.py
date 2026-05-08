1da3ab859458e064d3ed8f0342728c89930387b2
Jonas Souza <jonasmendes1301@gmail.com>
2026-05-08 22:52:05 +0100

    pagina inicial do admin

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('admin-pagina-inicial', views.admin_pagina_inicial, name='admin_pagina_inicial'),
 ]
--- catalogo/views.py
+++ catalogo/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
-# Create your views here.
+@login_required
+def admin_pagina_inicial(request):
+    if not request.user.is_admin():
+        return redirect('colecao')
+    return render(request, 'catalogo/admin_pagina_inicial.html')
