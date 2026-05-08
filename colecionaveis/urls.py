8218396d478512679c5cbb34e0eb6e5f8f894641
Jonas Souza <jonasmendes1301@gmail.com>
2026-05-08 22:54:01 +0100

    pagina inicial do utilizador

--- colecionaveis/urls.py
+++ colecionaveis/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('colecao/', views.colecao, name='colecao'),
 ]
--- colecionaveis/views.py
+++ colecionaveis/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+@login_required
+def colecao(request):
+    return render(request, 'colecionaveis/colecao.html')
