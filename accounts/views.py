aee686cf9fe2204fe731f0f9f5705f28352b5142
Jonas Souza <jonasmendes1301@gmail.com>
2026-05-08 22:48:41 +0100

    correçao redirecionamento do login por tipo de user (se admin ou utilizador) e ajustes de design

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,22 @@
-from django.contrib.auth import login, logout
+from django.contrib.auth import login as auth_login, logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 
-def login(request):
+def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
-            login(request, user)
-            return redirect('/')
+            auth_login(request, user)
+            if user.is_admin():
+                return redirect('/admin-pagina-inicial')
+            else:
+                return redirect('/colecao')
     else:
         form = AuthenticationForm()
     return render(request, 'accounts/login.html', {'form': form})
 
-def logout(request):
-    logout(request)
+def logout_view(request):
+    auth_logout(request)
     return redirect('/login/')
 
